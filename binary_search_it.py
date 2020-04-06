b67afed36a3e1176c62582c0f54f32b92d19054f
Miela Foster <mielafoster@Mielas-MacBook.local>
2020-04-05 22:50:35 -0400

    Commit

--- binary_search_it.py
+++ binary_search_it.py
@@ -23,7 +23,7 @@ def binary_search(array, first, last, value):
 
 #Now we want to be able to test this
 
-#let's first may an array
+#let's first make an array
 array = [1,2,3,4,5,6,7,8,9,10,11]
 value = 3
 
