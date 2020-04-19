ee518724c9322bdc69df96c0e26ccc6283af3ef9
Miela Foster <mielafoster@mielas-macbook.lan>
2020-04-18 21:36:16 -0400

    day 18!

---
+++ pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  int number = 10;
+  /*We denote the address of the integer with the %p symbol*/
+  printf("This is the addess of the integer %p", &number);
+  return 0;
+}
+
+/*Another variation of this is declaring the pointer*/
+
+int main(){
+  int number = 10;
+
+  int *p;
+  /*Where p is the address of the number*/
+
+  p =
+  
+  printf("This is the addess of the integer %p", p);
+  return 0
+}
